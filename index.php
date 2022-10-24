41d9b87011e4a7d43a9321efca134eb990e5dba6
Lazuardi-hrp <anakitekonologi@gmail.com>
2022-10-24 11:24:28 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h2>Hello World!</h2>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error minus magni in provident earum blanditiis aspernatur cum a dolores fugiat, eligendi aliquid commodi sint, alias necessitatibus quam. Eos, in nostrum.</p>
 </body>
 </html>
